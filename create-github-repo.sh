e02a67d6e0906083ef7006adc539e79a88a97f10
Brad Caron <bacaron245@gmail.com>
2022-04-02 14:41:04 -0400

    Reason: update with script to create github repos

--- create-github-repo.sh
+++ create-github-repo.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-startDir=pwd
+startDir=$(eval pwd)
 repopath=$1
 reponame=$2
 username=$3
 
-[ ! -d $repopath ] && mkdir - p $repopath && cd $repopath
+# make new repo directory
+[ ! -d $repopath ] && mkdir -p $repopath && cd $repopath
+
+# grab github public access key
+pubkey=`cat ~/.ssh/gitkeys`
 
 # git related functions
 git init # initialize the directory as a github repo
-cat "# $reponame" >> README.md # generate blank README
+echo "# $reponame" >> README.md # generate blank README
 git add . # add files
 git commit -m "reason: initial commit" # message describing updates
-curl -u $username https://api.github.com/user/repos -d '{"name":$reponame,"private":false}' # creates new repo
-git remote add origin https://github.com/$username/$reponame # adds remote repository as origin
+dict=`echo $(printf '{"name":"%q","private":false}' "$reponame")` # set up json dictionary to pass into curl command
+curl -u $username:$pubkey https://api.github.com/user/repos -d $dict # creates new repo
+eval git remote add origin https://github.com/$username/$reponame # adds remote repository as origin
+eval git remote set-url origin git@github.com:$username/$reponame.git # set url origin
 git branch -m master main # changes name of top branch from master to main
 git push -u origin main # pushes changes to main branch
 
